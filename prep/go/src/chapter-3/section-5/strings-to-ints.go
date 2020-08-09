4b3be8d1743ad4fb82ecac821e3b825d6afb5619
Jonathan Milgrom <jmilgrom@snapchat.com>
2020-08-09 13:44:24 -0400

    fun with strings and ints

--- prep/go/src/chapter-3/section-5/strings-to-ints.go
+++ prep/go/src/chapter-3/section-5/strings-to-ints.go
@@ -8,12 +8,14 @@ import (
 
 func main() {
 	a := "123"
-	i, _ := strconv.Atoi(a)
+	i, _ := strconv.Atoi(a) // string to int through strconv.Atio
 	fmt.Printf("%d\n", i)
+	x, _ := strconv.ParseInt(a, 10, 64) // string to string through strconv.ParseInt
+	fmt.Println(x)
 
 	var buf bytes.Buffer
 	fmt.Fprintf(&buf, "%d", i)
-	fmt.Println(buf.String())
+	fmt.Println(buf.String()) // int to string through buffer.String
 
-	fmt.Printf("%b\n", i)
+	fmt.Printf("%b\n", i) // int to binary
 }
