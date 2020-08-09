15f46181f41ee056ec4147c0b9392af97ade664a
Jonathan Milgrom <jmilgrom@snapchat.com>
2020-08-09 13:47:00 -0400

    denote task

--- prep/go/src/chapter-3/section-5/strings-to-ints.go
+++ prep/go/src/chapter-3/section-5/strings-to-ints.go
@@ -7,15 +7,19 @@ import (
 )
 
 func main() {
+	println("### string to int")
 	a := "123"
 	i, _ := strconv.Atoi(a) // string to int through strconv.Atio
 	fmt.Printf("%d\n", i)
 	x, _ := strconv.ParseInt(a, 10, 64) // string to string through strconv.ParseInt
 	fmt.Println(x)
 
+	println("### int to string")
+	fmt.Printf("%d\n", i) // int to a string
 	var buf bytes.Buffer
 	fmt.Fprintf(&buf, "%d", i)
 	fmt.Println(buf.String()) // int to string through buffer.String
 
+	println("### misc")
 	fmt.Printf("%b\n", i) // int to binary
 }
