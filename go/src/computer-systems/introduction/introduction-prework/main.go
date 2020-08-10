b8672194ca77e01d6df68812280742e849f0abf2
Jonathan Milgrom <jmilgrom@snapchat.com>
2020-08-10 19:07:09 -0400

    clean up

--- go/src/computer-systems/introduction/introduction-prework/main.go
+++ go/src/computer-systems/introduction/introduction-prework/main.go
@@ -7,6 +7,23 @@ import (
 
 func main() {
 	var memory = make([]byte, 256)
+	memory[1] = 3
+	memory[2] = 5
 	memory[8] = vm.Load
-	fmt.Printf("Load instruction test %s", vm.Compute(memory))
+	memory[9] = byte(vm.R1)
+	memory[10] = 0x02
+	memory[11] = vm.Store
+	memory[12] = byte(vm.R1)
+	memory[13] = 0x00
+	memory[14] = vm.Halt
+
+	vm.Compute(memory)
+
+	printMemory(memory[:20])
+}
+
+func printMemory(memory []byte) {
+	for i, b := range memory {
+		fmt.Printf("%d: %x\n", i, b)
+	}
 }
--- go/src/computer-systems/introduction/introduction-prework/vm/vm.go
+++ go/src/computer-systems/introduction/introduction-prework/vm/vm.go
@@ -1,11 +1,27 @@
 package vm
 
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
 const (
-	Load  = 0x01
+	// Load a value into memory
+	// usage: Load R1 0x01 		# Load data from byte 1 into register 1
+	Load = 0x01
+	// Store a value in the output byte (0x00) of memory from a register
+	// usage: Store R1 0x00     # Place the value in R1 into the output location
 	Store = 0x02
-	Add   = 0x03
-	Sub   = 0x04
-	Halt  = 0xff
+	// Add two values and store in first
+	// usage: add R1 R2       	# Add the two register values, store the result in r1
+	Add = 0x03
+	// Sub (tract) two values and store in first
+	// usage: sub R1 R2			# Set r1 = r1 - r2
+	Sub = 0x04
+	// Halt program
+	// usage: halt
+	Halt = 0xff
 )
 
 // Stretch goals
@@ -16,6 +32,39 @@ const (
 	Beqz = 0x08
 )
 
+// Register is a enumeration of register addresses
+type register int
+
+const (
+	// PC is pogram counter
+	PC register = iota
+	// R1 is the first general purpose register
+	R1
+	// R2 is the second general purpose register
+	R2
+)
+
+type arity int
+
+const (
+	nullary arity = iota
+	unary
+	binary
+)
+
+var instructionArity = map[byte]arity{
+	Load:  binary,
+	Store: binary,
+	Add:   binary,
+	Sub:   binary,
+	Halt:  nullary,
+}
+
+// OutputAddress is the address reserved for the progam output
+const OutputAddress = 0x00
+
+// Compute the result of a set of instructions
+//
 // Given a 256 byte array of "memory", run the stored program
 // to completion, modifying the data in place to reflect the result
 //
@@ -25,29 +74,69 @@ const (
 // __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ ... __
 // ^==DATA===============^ ^==INSTRUCTIONS==============^
 //
-func Compute(memory []byte) string {
+func Compute(memory []byte) {
 
-	type Instruction int
+	registers := [3]byte{
+		PC: 8, // Program Counter: address of next instruction
+		R1: 0, // R1: a value
+		R2: 0, // R2: a value
+	}
 
-	const (
-		instruction Instruction = iota
-		arg1
-		arg2
-	)
-	registers := [3]byte{8, 0, 0} // PC, R1 and R2
+	maxAddress := cap(memory) - 1
 
 	// Keep looping, like a physical computer's clock
 	for {
+		address := int(registers[PC])
+		if address > maxAddress {
+			return
+		}
 
-		op := memory[registers[instruction]]
+		op := memory[address]
 
 		// decode and execute
-		switch op {
-		case Load:
-
-			//   TODO
-			// ...
-			return "hey"
+		switch instructionArity[op] {
+		case nullary:
+			switch op {
+			case Halt:
+				registers[PC]++
+				return
+			}
+		case binary:
+			a, b, err := fetchTwo(memory, address, maxAddress)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, err.Error())
+				return
+			}
+			handleBinaryInstructions(&registers, memory, op, a, b)
+		default:
+			fmt.Fprintf(os.Stderr, "Unsupported operation %x\n", op)
+			return
 		}
 	}
+}
+
+func handleBinaryInstructions(registers *[3]byte, memory []byte, op byte, a byte, b byte) {
+	switch op {
+	case Load:
+		register, dataAddress := a, b
+		registers[register] = memory[dataAddress]
+	case Store:
+		register, _ := a, b
+		memory[OutputAddress] = registers[register]
+	case Add:
+		registerA, registerB := a, b
+		registers[registerA] += registers[registerB]
+	case Sub:
+		registerA, registerB := a, b
+		registers[registerA] -= registers[registerB]
+	}
+	registers[PC] += 3
+}
+
+func fetchTwo(memory []byte, start int, max int) (byte, byte, error) {
+	second := start + 2
+	if second > max {
+		return 0, 0, errors.New("memory allocation exceeded")
+	}
+	return memory[start+1], memory[second], nil
 }
