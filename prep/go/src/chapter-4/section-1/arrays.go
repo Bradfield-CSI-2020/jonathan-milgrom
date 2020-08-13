1aa97b94e68c4b3267d48c715baa430ab8b45a30
Jonathan Milgrom <jmilgrom@snapchat.com>
2020-08-13 07:32:38 -0400

    messing with array

--- prep/go/src/chapter-4/section-1/arrays.go
+++ prep/go/src/chapter-4/section-1/arrays.go
@@ -23,16 +23,18 @@ func main() {
 
 	var archetypal2 = [100]int{}
 
-	for i := 0; i < len(archetypal2); i++ {
-		archetypal2[i] = i
-	}
 	fmt.Printf("Array literal archetypal[45]: %d\n", archetypal2[45])
 
 	var a = [...]string{usd: "$", gb: "g", eur: "e"}
 	fmt.Println(a[2]) // => g
 
-	var b = [...]int{99: -1}
+	var b = [...]int{99: 0}
 	fmt.Println(b[45]) // => 0
+	fmt.Printf("typeof [...]int{99: 0}: %T \n", b)
+	fmt.Printf("typeof [100]int{}: %T \n", archetypal2)
+	fmt.Printf("var archetypal [100]int: %T \n", archetypal)
+	fmt.Printf("[...]int{99: 0} == [100]int{}: %t \n", b == archetypal2)
+	fmt.Printf("[100]int == [100]int{}: %t \n", archetypal == archetypal2)
 
 	var c = [100]int{1, 2, 3}
 	fmt.Println(c[1])  // => 2
