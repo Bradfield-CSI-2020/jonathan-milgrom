afbb0fcc89358d013d3cd2e19d675f4485a8527e
Jonathan Milgrom <jmilgrom@snapchat.com>
2020-08-17 07:23:39 -0400

    clean up

--- assembly-prework/sum_to_n.asm
+++ assembly-prework/sum_to_n.asm
@@ -1,16 +1,13 @@
 section .text
 global sum_to_n
 sum_to_n:
-	mov rax, rdi	; rax is used as the total (and will be returned from func)
-	mov rdx, rax	; rdx is the next increment; set the next increment equal to the arg n
-	jmp compare
-
+			mov rax, rdi	; rax is used as the total (and will be returned from func)
+			mov rdx, rax	; rdx is the next increment; set the next increment equal to the arg n
 compare:
-	cmp rdx, 0	; compare the next increment to zero
-	jnz sum_it_up	; if not zero, make money moves
-	ret 	; otherwise return, we done
-
+			cmp rdx, 0		; compare the next increment to zero
+			jnz sum_it_up	; if not zero, iterate
+			ret 			; otherwise return, we done
 sum_it_up:
-	sub rdx, 1	; subtract one from our increment to form next increment
-	add rax, rdx	; increment total
-	jmp compare	; iterate
+			sub rdx, 1		; subtract one from our increment to form next increment
+			add rax, rdx	; increment total
+			jmp compare		; iterated
