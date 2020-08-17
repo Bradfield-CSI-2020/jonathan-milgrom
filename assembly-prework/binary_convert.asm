1508131c2712fbd0128bd143b389f4ecc639de65
Jonathan Milgrom <jmilgrom@snapchat.com>
2020-08-17 12:01:11 -0400

    slog through it

--- assembly-prework/binary_convert.asm
+++ assembly-prework/binary_convert.asm
@@ -1,4 +1,21 @@
 section .text
 global binary_convert
+
+; rdi points at the address of the first character of the string
+
 binary_convert:
-	ret
+				xor rdx, rdx				; index = 0 (rdx)
+				xor rax, rax				; sum = 0 (rax)
+compare:	
+				movzx rsi, byte [rdi + rdx]	; c = s[index] (rsi)
+				mov r8, rsi 				; i = c; going to calculate integer value
+				sub r8, 48 					; subtract ascii of 0 (48), order to get value of c
+				cmp rsi, 0					; NUL character is 0
+				jnz convert
+				ret
+convert:
+				imul rax, 2					; increase order of magnituted of sum
+				add rax, r8					; add newest value to sum
+				inc rdx						; index++
+				jmp compare
+
