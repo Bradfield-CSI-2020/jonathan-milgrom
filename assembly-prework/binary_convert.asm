e6a1487aed8edb7cd757ce06f58b15fe919df8a8
Jonathan Milgrom <jmilgrom@snapchat.com>
2020-08-17 12:02:54 -0400

    slog through it

--- assembly-prework/binary_convert.asm
+++ assembly-prework/binary_convert.asm
@@ -4,18 +4,18 @@ global binary_convert
 ; rdi points at the address of the first character of the string
 
 binary_convert:
-				xor rdx, rdx				; index = 0 (rdx)
-				xor rax, rax				; sum = 0 (rax)
+				xor rdx, rdx		; index = 0 (rdx)
+				xor rax, rax		; sum = 0 (rax)
 compare:	
 				movzx rsi, byte [rdi + rdx]	; c = s[index] (rsi)
-				mov r8, rsi 				; i = c; going to calculate integer value
-				sub r8, 48 					; subtract ascii of 0 (48), order to get value of c
-				cmp rsi, 0					; NUL character is 0
+				mov r8, rsi 		; i = c; going to calculate integer value
+				sub r8, 48 			; subtract ascii of 0 (48), order to get value of c
+				cmp rsi, 0			 NUL character is 0
 				jnz convert
 				ret
 convert:
-				imul rax, 2					; increase order of magnituted of sum
-				add rax, r8					; add newest value to sum
-				inc rdx						; index++
+				imul rax, 2			; increase order of magnituted of sum
+				add rax, r8			; add newest value to sum
+				inc rdx				; index++
 				jmp compare
 
