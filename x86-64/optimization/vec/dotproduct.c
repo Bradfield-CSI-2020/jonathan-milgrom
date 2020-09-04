dc4f6846544e6274003eee0d179a2f1639d9e3d8
Jonathan Milgrom <jmilgrom@snapchat.com>
2020-09-04 07:06:12 -0700

    optimization starter

---
+++ x86-64/optimization/vec/dotproduct.c
@@ -0,0 +1,14 @@
+#include "vec.h"
+
+
+data_t dotproduct(vec_ptr u, vec_ptr v) {
+   data_t sum = 0, u_val, v_val;
+   int l = vec_length(u);
+
+   for (long i = 0; i < l; i++) { // we can assume both vectors are same length
+        get_vec_element(u, i, &u_val);
+        get_vec_element(v, i, &v_val);
+        sum += u_val * v_val;
+   }   
+   return sum;
+}
